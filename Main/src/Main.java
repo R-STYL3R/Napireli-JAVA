81cc120c125af51485e01ce03279ba6af2145340
R-STYL3R <tor.vipo@gmail.com>
2024-12-20 18:31:22 +0400

    Num and Name update

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+    System.out.println("Write Number:"+"/t");
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    if(number>7){
+        System.out.println("Hello");
+    }
+
+    System.out.println("Write Name:"+"/t");
+    String name = sc.next();
+    Name Name = new Name(name);
+    Name.Match();
+
+
+    }
+}
+
+class Name{
+    String name;
+
+    public Name(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void Match(){
+        if(name.equals("Jhon")){
+            System.out.println("Hello, Jhon");
+        }else {
+            System.out.println("There is no such name");
+        }
     }
 }
