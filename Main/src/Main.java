411d73ec511681614f2f6b2214bc09bf411da390
R-STYL3R <tor.vipo@gmail.com>
2024-12-20 18:58:46 +0400

    Finale version

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,19 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    System.out.println("Write Number:"+"/t");
-    Scanner sc = new Scanner(System.in);
-    int number = sc.nextInt();
-    if(number>7){
-        System.out.println("Hello");
-    }
+        System.out.println("Write Number:" + "\t");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 7) {
+            System.out.println("Hello" + "\t");
+        }
 
-    System.out.println("Write Name:"+"/t");
-    String name = sc.next();
-    Name Name = new Name(name);
-    Name.Match();
+        System.out.println("Write Name:" + "\t");
+        String name = sc.next();
+        Name Name = new Name(name);
+        Name.Match();
 
-
+        int[] array;
+        System.out.println("Write the number of elements in the array:" + "\t");
+        int arrNum = sc.nextInt();
+        array = new int[arrNum];
+        int i;
+        for (i = 0; i < array.length; i++) {
+            System.out.println("Write the element in the array:" + "\t");
+            int eleNum = sc.nextInt();
+            array[i] = eleNum;
+        }
+        System.out.println("Elements which are are multiples of 3:" + "\t");
+        for (i = 0; i < array.length; i++) {
+            if (array[i]%3==0) {
+                System.out.println(array[i]+" ");
+            }
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Napireli-JAVA
+Completed technical task for the traineeship candidates of Anderson.
